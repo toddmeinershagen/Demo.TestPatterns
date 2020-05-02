e8da6b36c03e631f26303dbf45821abbcf73d501
Todd Meinershagen <todd@meinershagen.net>
2020-05-01 23:37:49 -0400

    Added an example of a decorative pattern for doing something either before or after an HttpClient call.

---
+++ src/Demo.TestPatterns/Demo.TestPatterns.Tests/TestsWithDecoratedHttpClient.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestsWithDecoratedHttpClient
+{
+    private static Lazy<HttpClient> Client = new Lazy<HttpClient>(() => {
+        var client = HttpClientFactory.Create(new DecorativeHandler());
+        client.BaseAddress = new Uri("https://postman-echo.com/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        return client;
+    });
+
+    [Test]
+    public async Task test_with_decorative_call_to_http_client()
+    {
+        var response = await Client.Value.GetAsync("/get?name=Todd");
+        var echo = await response.Content.ReadAsAsync<EchoResponse>();
+        
+        echo.Args.Name.Should().Be("Todd");
+        echo.Url.Should().Be("https://postman-echo.com/get?name=Todd");
+    }
+
+    [Test]
+    public async Task test_with_decorative_call_to_http_client_too()
+    {
+        var response = await Client.Value.GetAsync("/get?name=Todd");
+        var echo = await response.Content.ReadAsAsync<EchoResponse>();
+        
+        echo.Args.Name.Should().Be("Todd");
+        echo.Url.Should().Be("https://postman-echo.com/get?name=Todd");
+    }
+
+    public class EchoResponse
+    {
+        public EchoArgs Args { get; set;}
+        public string Url { get; set; }
+
+        public class EchoArgs
+        {
+            public string Name { get; set;}
+        }
+    }
+}
+
+public class DecorativeHandler : DelegatingHandler
+{
+    protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage, CancellationToken cancellationToken)
+    {
+        await Console.Out.WriteLineAsync($"{TestContext.CurrentContext.Test.Name}:  Before the Http call.");
+        var response = await base.SendAsync(requestMessage, cancellationToken);
+        await Console.Out.WriteLineAsync($"{TestContext.CurrentContext.Test.Name}:  After the Http call.");
+        
+        return response;
+    }
+}
