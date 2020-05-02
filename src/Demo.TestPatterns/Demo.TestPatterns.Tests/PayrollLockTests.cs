7f9f7be77628101cc502fd48d1e56a9bdf856bbf
Todd Meinershagen <todd@meinershagen.net>
2020-05-01 22:21:51 -0400

    Introduced parallelization techniques with nunit.

--- src/Demo.TestPatterns/Demo.TestPatterns.Tests/PayrollLockTests.cs
+++ src/Demo.TestPatterns/Demo.TestPatterns.Tests/PayrollLockTests.cs
@@ -1,33 +1,41 @@
 using NUnit.Framework;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Demo.TestPatterns.Tests
 {
-    public class Tests
+    [Parallelizable(ParallelScope.Children)]
+    public class PayrollLockTests
     {
-        [Test]
+        [TestCase()]
+        [TestCase()]
+        [TestCase()]
         public async Task given_payrun_locked_when_performing_an_action_successfully_then_payrun_unlocked()
         {
-            var output = Console.Out;
-            var payRunId = 1200;
+            var payRunId = 1201;
 
-            await using (var myLock = new PayrollLock(output, payRunId))
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            await using (var myLock = new PayrollLock(Console.Out, payRunId))
             {
-                await output.WriteLineAsync($"Performing action for pay run id {payRunId}.");
+                await Console.Out.WriteLineAsync($"Performing action for pay run id {payRunId}.");
             }
         }
 
-        [Test]
+        [TestCase()]
+        [TestCase()]
+        [TestCase()]
         public async Task given_payrun_locked_when_performing_an_action_with_exception_then_payrun_unlocked()
         {
-            var output = Console.Out;
             var payRunId = 1200;
 
-            await using (var myLock = new PayrollLock(output, payRunId))
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            await using (var myLock = new PayrollLock(Console.Out, payRunId))
             {
-                await output.WriteLineAsync($"Performing action for pay run id {payRunId}.");
-                Assert.Fail("Something went wrong when performing action for pay run id");
+                await Console.Out.WriteLineAsync($"Performing action for pay run id {payRunId}.");
+                Assert.Fail($"Something went wrong when performing action for pay run id {payRunId}.");
             }
         }
     }
